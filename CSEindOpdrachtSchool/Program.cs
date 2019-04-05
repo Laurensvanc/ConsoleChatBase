91bc6379929d032c5d672f03bf30984b225e68ed
laurensvanc <laurensvancoeverden@gmail.com>
2019-04-05 15:37:47 +0200

    Main Program

---
+++ CSEindOpdrachtSchool/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSEindOpdrachtSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Laurens Console.");
+            Console.WriteLine("Press any key to start the program.");
+            Console.ReadKey();
+
+            Console.Clear();
+
+            Console.WriteLine("Please enter your Username.");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Username is: " + userName);
+
+
+            Console.ReadKey();
+        }
+    }
+}
